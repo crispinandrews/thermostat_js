0a736c2aec121601495e77df9f59a248de793f77
crispinandrews <crispin.andrews@gmail.com>
2016-10-25 18:06:49 +0100

    energy usage sepcs added and tests passing

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,11 +1,13 @@
 'use strict';
 
 function Thermostat(){
-  this.temperature = 20;
+  this.DEFAULT_TEMPERATURE = 20;
+  this.temperature = this.DEFAULT_TEMPERATURE;
   this.MIN_TEMP = 10;
   this.MAX_LIMIT_PSM_ON = 25;
   this.MAX_LIMIT_PSM_OFF = 32;
   this.powerSavingMode = true;
+  this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
 }
 
 Thermostat.prototype.isPowerSavingModeOn = function(){
@@ -48,3 +50,17 @@ Thermostat.prototype.isMaximumTemperature = function() {
   }
   return this.temperature === this.MAX_LIMIT_PSM_ON;
 };
+
+Thermostat.prototype.resetTemperature = function(){
+  this.temperature = this.DEFAULT_TEMPERATURE;
+};
+
+Thermostat.prototype.energyUsage = function() {
+  if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+    return 'low-usage';
+  }
+  if (this.temperature >= this.MEDIUM_ENERGY_USAGE_LIMIT && this.temperature <= this.MAX_LIMIT_PSM_ON) {
+    return 'medium-usage';
+  }
+  return 'high-usage';
+}
